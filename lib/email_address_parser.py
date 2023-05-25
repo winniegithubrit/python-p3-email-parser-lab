26e5d627d03c631df0ceed0061cb841248f5d234
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-25 12:59:52 +0300

    Completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -8,7 +8,7 @@ class EmailAddressParser:
         self.email_address = email_address
 
     def parse(self):
-        pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+        pattern = r"\b[A-Za-z0-9.]+@[A-Za-z0-9.-]+\.[A-Za-z]{3,}\b"
         email = re.findall(pattern, self.email_address)
 
         email.sort()

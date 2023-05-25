11731debd90e356ecf491b79dcd5a79c6b104722
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-25 12:46:54 +0300

    Completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,19 @@
 # your code goes here!
+
+
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_address):
+        self.email_address = email_address
+
+    def parse(self):
+        pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+        email = re.findall(pattern, self.email_address)
+
+        email.sort()
+
+        return email
+
+
+  
